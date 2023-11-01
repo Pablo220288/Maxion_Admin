c2163e5a72442c5fb1997ecfe71580596b0659f8
Pab Her <peh_tj@hotmail.com>
2023-11-01 12:53:41 -0300

    TableStaff Delete

--- pages/staff.js
+++ pages/staff.js
@@ -54,7 +54,7 @@ export default function Staff() {
           </svg>
         </Link>
       </div>
-      {allStaff.length > 0 && <TableStaff allStaff={allStaff} />}
+      {allStaff.length > 0 && <div>{allStaff[0].name}</div>}
     </Layout>
   );
 }
